68b6038aa7d763a37df681d9138f148254be99c2
airubb <128812023+airubb@users.noreply.github.com>
2026-06-25 16:51:40 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,14 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: "/culture-lab/",
+
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile()
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
